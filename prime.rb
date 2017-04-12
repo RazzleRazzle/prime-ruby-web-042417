7da22175fa9f21522eebfaf007619f1dea15c337
Jake Musich <makejusich@gmail.com>
2017-04-12 16:29:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(num)
+if num <= 1
+    return false
+end
+
+#for i in 2..(num.abs - 1)
+(2...num.abs).each do |i|
+  if num % i == 0
+    return false
+  end
+end
+  return true
+end
